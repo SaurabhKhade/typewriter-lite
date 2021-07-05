43e2d8b9933b42e3f7faf19dd00d1d69f5d99c49
Saurabh Khade <khadesaurabh056@gmail.com>
2021-07-05 11:56:58 +0530

    v1.0.0

---
+++ index.js
@@ -0,0 +1,50 @@
+function animatedTyping(params) {
+
+  let {
+    element,
+    texts_array,
+    write_delay = 500,
+    remove_delay = 500,
+    write_speed_per_char = 100,
+    remove_speed_per_char = 100
+  } = params;
+
+  let text_index = 0;
+  let character_index = 0;
+  let text_to_write;
+  
+  element.innerText = "";
+  
+  function text_typer() {
+
+    text_to_write = texts_array[text_index];
+    let characters_in_text = text_to_write.length;
+    let character_typer_interval = setInterval(character_typer, write_speed_per_character);
+    
+    function character_typer() {
+      element.innerText += text_to_write[character_index++];
+      if (character_index === characters_in_text) {
+        clearInterval(character_typer_interval);
+        setTimeout(text_remover, remove_delay);
+      }
+    }
+  }
+  
+  function text_remover() {
+    let character_remover_interval = setInterval(character_remover, remove_speed_per_character);
+    
+    function character_remover() {
+      element.innerText = text_to_write.slice(0, --character_index);
+
+      if (character_index === 0) {
+        clearInterval(character_remover_interval);
+        text_index = (text_index+1)%texts_array.length;
+        setTimeout(text_typer, write_delay);
+      }
+    }
+  }
+  
+  setTimeout(text_typer, write_delay);
+}
+
+module.exports = animatedTyping
